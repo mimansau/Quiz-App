2c7c3889d912a0d4be83e93d85222158f6dc0c0c
mimansau <mimansaupadhyay10@gmail.com>
2023-10-09 14:46:11 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Quiz_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(121, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "NEXT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(91, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 35);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "QUIZ APP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Question";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(88, 187);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 233);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(88, 278);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(88, 323);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(103, 20);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(157, 463);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 581);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_App
+{
+    public partial class Form1 : Form
+    {
+        private List<QuizQuestion> questions;
+        private int currentQuestionIndex = 0;
+        private int score = 0;
+        private RadioButton[] radioButtonOptions;
+        public Form1()
+        {
+            InitializeComponent();
+
+            radioButtonOptions = new RadioButton[]
+            {
+                radioButton1,
+                radioButton2,
+                radioButton3,
+                radioButton4,
+            
+            };
+
+            questions = new List<QuizQuestion>
+            {
+                new QuizQuestion
+                {
+                    QuestionText = "What is the capital of France?",
+                    Choices = new List<string> { "Paris", "London", "Berlin", "Madrid" },
+                    CorrectAnswerIndex = 0
+                },
+                new QuizQuestion
+                {
+                    QuestionText = "Which planet is known as the Red Planet?",
+                    Choices = new List<string> { "Earth", "Mars", "Venus", "Jupiter" },
+                    CorrectAnswerIndex = 1
+                },
+                // Add more questions here
+            };
+
+            // Initialize the first question
+            DisplayQuestion();
+        }
+
+        private void DisplayQuestion()
+        {
+            if (currentQuestionIndex < questions.Count)
+            {
+                QuizQuestion currentQuestion = questions[currentQuestionIndex];
+                label2.Text = currentQuestion.QuestionText;
+
+                for (int i = 0; i < currentQuestion.Choices.Count; i++)
+                {
+                    radioButtonOptions[i].Text = currentQuestion.Choices[i];
+                }
+            }
+            else
+            {
+                // Quiz finished, display the final score
+                label4.Text = "Quiz Finished!";
+                MessageBox.Show($"Your Score: {score}/{questions.Count}", "Quiz Finished");
+            }
+        }
+
+        public class QuizQuestion
+        {
+            public string QuestionText { get; set; }
+            public List<string> Choices { get; set; }
+            public int CorrectAnswerIndex { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButtonOptions[currentQuestionIndex].Checked)
+            {
+                QuizQuestion currentQuestion = questions[currentQuestionIndex];
+                if (radioButtonOptions[currentQuestionIndex].Text == currentQuestion.Choices[currentQuestion.CorrectAnswerIndex])
+                {
+                    score++; // Correct answer
+                }
+            }
+
+            // Move to the next question
+            currentQuestionIndex++;
+
+            // Display the next question or finish the quiz
+            DisplayQuestion();
+        }
+    }
+}
